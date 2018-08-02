66985aa7422b9db360777f16fc3b343f911aa13c
Jon Cokisler <can.cokisler@gmail.com>
2018-08-02 15:34:43 -0400

    alpha 0.3

--- src/Ball.java
+++ src/Ball.java
@@ -2,19 +2,20 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Ball{
 	public int numberOfBalls;
 	public int xRange;
 	public int yRange;
-	public int x;
-	public int y;
+	public static int x;
+	public static int y;
 	public int radius;
 	public int numberRandX;
 	public int numberRandY;
 	public int arrayX[];
 	public int arrayY[];
+	
 	public Ball() {
 			Random random = new Random();
 			
@@ -22,11 +23,15 @@ public class Ball{
 			this.y = random.nextInt(1000);		
 	}
 	
-	public Graphics display(Graphics g) {
+	
+	
+	public void display(Graphics g) {
 		
-		
-			g.drawOval(x, y, radius, radius);
 			g.setColor(Color.WHITE);
-			return g;
+			
+			g.fillOval(x, y, 50, 50);
+			
+			
 	}
+	
 }
--- src/Display.java
+++ src/Display.java
@@ -1,25 +1,54 @@
 import java.awt.Color;
-import java.awt.Graphics;
 
-import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Display extends JPanel {
+	public int DirX;
+	public int DirY;
+	
+	Timer refresher = new Timer(16, new ActionListener() {
 
-	public Display() {		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			update();
+			
+			
+			
+		}});
+	public Display() {
 		
 	}
-	
+
 	public void paintComponent(Graphics g) {
-//		g.fillRect(100,100, 1000, 1000);
-		super.paintComponent(g);
+		// g.fillRect(100,100, 1000, 1000);
 		this.setBackground(Color.BLACK);
-		Ball ball = new Ball();
-		super.paintComponent(ball.display(g));
-	}
+		super.paintComponent(g);
+
+		for (int i = 0; i <= 20; i++) {
+			Ball ball = new Ball();
+			ball.display(g);
+		}
 		
+		
+	}
 	
-	
-	
+	public void update() {
+		Random ballRand = new Random();
+		this.DirX = ballRand.nextInt(100);
+		this.DirY = ballRand.nextInt(100);
+		
+		//just random numbers to see if balls are moving or not
+		Ball.x += 100;
+		Ball.y +=100;
+		repaint();
+		
+		
+	}
 
 }
